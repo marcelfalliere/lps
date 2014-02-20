d7f2628af5a8e9056293c751d846ce9b2297a920
Frédéric Falliere <frederic.falliere@gmail.com>
2014-02-20 07:26:01 +0100

    before plugin stream camera

--- phonegap/www/js/app/conf.js
+++ phonegap/www/js/app/conf.js
@@ -1,7 +1,8 @@
 (function(){
 	window.conf = {
 		server : {
-			base_url:'http://192.168.1.21:9999/'
+			//base_url:'http://192.168.1.21:9999/',
+			base_url:'http://62.210.237.246:9999/'
 		}
 	}
 
